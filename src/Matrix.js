58ea9355cd43cdb9d098205b97872a8a494933fc
Mikah Niehaus <Graywolf.niehaus@live.com>
2020-08-12 16:31:44 -0500

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -3,16 +3,21 @@ import Cell from './Cell.js';
 
 export default class Matrix extends Component {
   
-  genRow = (vals) => {
+  genRow = (vals) => //adds rows
+   {
     //return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
     return vals.map(val => <Cell value={val} />)
   }
-  
-  genMatrix = () => {
+  //function
+  genMatrix = () => //adds collums
+  {
+    //The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
+    //this.props is defalt props
     return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   }
   
-  render() {
+  render()//returns all
+   {
     return (
       <div id="matrix">
         {this.genMatrix()}
@@ -21,6 +26,7 @@ export default class Matrix extends Component {
   }
   
 }
+// The defaultProps will be used to ensure that this.props.name will have a value if it was not specified by the parent component. 
 Matrix.defaultProps = {
   values: [
     ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ import './index.css';
 import { pattern1 } from './data.js'
 import Matrix from './Matrix.js'
 
-ReactDOM.render(<Matrix values={pattern1} />, document.getElementById('root'));
+//ReactDOM.render() controls the contents of the container node you pass in. 
+//values={pattern1} is the node getting passed
+//getElementById() returns an Element object representing the element whose id property matches the specified string.
+ReactDOM.render(<Matrix values={pattern1} />, document.getElementById('root')); //i am rendering the thing in Matrix
